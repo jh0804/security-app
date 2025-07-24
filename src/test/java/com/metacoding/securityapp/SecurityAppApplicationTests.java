c49c310ae758fba92814bce3b08de8369c6c12fb
JH <wngml0708@gmail.com>
2025-07-24 16:49:59 +0900

    Spring Security SSR

---
+++ src/test/java/com/metacoding/securityapp/SecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metacoding.securityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
